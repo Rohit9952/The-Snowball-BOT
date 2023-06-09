54a904bfb4c30404019e770c68ea6f8e464a5e15
RITESH <108003920+ritesh-0@users.noreply.github.com>
2023-06-09 23:44:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = 'mongodb+srv://CAPTAINSNOWBALL:CAPTAINSNOWBALL@cluster0.gqhoqih.mongodb.net/?retryWrites=true&w=majority'
+DATABASE_URI = 'mongodb+srv://SNOWBALL:<password>@cluster0.yfeei9p.mongodb.net/?retryWrites=true&w=majority'
 DATABASE_NAME = 'Cluster0'
 COLLECTION_NAME = 'Media_Files'
 
